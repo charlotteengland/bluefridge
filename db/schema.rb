0a70cff32c1f8e46c2375cae83cc1988ce856759
charlotteengland <charlotteyandell88@gmail.com>
2017-03-18 13:08:26 +0000

    added integer type to products prices

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170311170507) do
+ActiveRecord::Schema.define(version: 20170318130443) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
