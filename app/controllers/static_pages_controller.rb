41a6f843fc6eb2f1b14a0ec7d51b6210f70151f2
charlotteengland <charlotteyandell88@gmail.com>
2017-03-19 15:36:14 +0000

    4.3 migrate db test

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
-  def index
-  end
- 
+   def landing_page
+   	@featured_product = product.first
+   end
 
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@ Rails.application.routes.draw do
 
   get 'static_pages/about'
 
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
+
 
 
 
